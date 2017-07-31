4a5c6a851fcc6468c8301fae00bca0d1f85ae355
Justin Svegliato <justin.svegliato@gmail.com>
2017-07-31 16:39:25 -0400

    Changed params

--- experiments.py
+++ experiments.py
@@ -5,13 +5,14 @@ import computation
 import monitor
 import tsp
 import performance
+import matplotlib.pyplot as plt
 import utils
 
 import numpy as np
 
-TIME_COST_MULTIPLIER = 0.1
+TIME_COST_MULTIPLIER = 0.15
 INTRINSIC_VALUE_MULTIPLIER = 200
-SOLUTION_QUALITY_CLASS_COUNT = 5
+SOLUTION_QUALITY_CLASS_COUNT = 20
 SOLUTION_QUALITY_CLASS_BOUNDS = np.linspace(0, 1, SOLUTION_QUALITY_CLASS_COUNT + 1)
 SOLUTION_QUALITY_CLASSES = range(SOLUTION_QUALITY_CLASS_COUNT)
 
@@ -22,6 +23,8 @@ CONFIG = {
     "quality_class_bounds": SOLUTION_QUALITY_CLASS_BOUNDS,
     "quality_class_count": SOLUTION_QUALITY_CLASS_COUNT
 }
+
+TOUR_SIZE = 50
 
 
 def get_optimal_costs(file_path):
@@ -80,7 +83,8 @@ def experiment_1(simulations_file, instances_directory):
             file_path = os.path.join(instances_directory, file)
 
             true_qualities = simulations[instance]["qualities"]
-            true_comprehensive_values = computation.get_time_dependent_utility(true_qualities, range(len(true_qualities)), INTRINSIC_VALUE_MULTIPLIER, TIME_COST_MULTIPLIER)
+            true_steps = range(len(true_qualities))
+            true_comprehensive_values = computation.get_time_dependent_utility(true_qualities, true_steps, INTRINSIC_VALUE_MULTIPLIER, TIME_COST_MULTIPLIER)
 
             optimal_stopping_point = computation.get_optimal_stopping_point(true_comprehensive_values)
             optimal_comprehensive_value = true_comprehensive_values[optimal_stopping_point]
@@ -92,23 +96,73 @@ def experiment_1(simulations_file, instances_directory):
 
             print("Fixed Monitor:")
             _, fixed_records = monitor.fixed_monitor(tsp.k_opt_solve, quality_estimator, profile_4, CONFIG, states, start_state, 1000)
-            fixed_comprehensive_value = computation.get_time_dependent_utility(fixed_records[-1]['q'], fixed_records[-1]['t'], INTRINSIC_VALUE_MULTIPLIER, TIME_COST_MULTIPLIER)
+            fixed_qualities = [record['q'] for record in fixed_records]
+            fixed_points = [record['t'] for record in fixed_records]
+            fixed_comprehensive_values = computation.get_time_dependent_utility(fixed_qualities, fixed_points, INTRINSIC_VALUE_MULTIPLIER, TIME_COST_MULTIPLIER)
+            fixed_quality = fixed_records[-1]['q']
+            fixed_point = fixed_records[-1]['t']
+            fixed_comprehensive_value = computation.get_time_dependent_utility(fixed_quality, fixed_point, INTRINSIC_VALUE_MULTIPLIER, TIME_COST_MULTIPLIER)
             fixed_losses.append(utils.get_percent_error(optimal_comprehensive_value, fixed_comprehensive_value))
 
             print("Myopic Monitor:")
             _, myopic_records = monitor.myopic_monitor(tsp.k_opt_solve, quality_estimator, profile_1, profile_3, CONFIG,  states, start_state, 1000)
-            myopic_comprehensive_value = computation.get_time_dependent_utility(myopic_records[-1]['q'], myopic_records[-1]['t'], INTRINSIC_VALUE_MULTIPLIER, TIME_COST_MULTIPLIER)
+            myopic_qualities = [record['q'] for record in myopic_records]
+            myopic_points = [record['t'] for record in myopic_records]
+            myopic_comprehensive_values = computation.get_time_dependent_utility(myopic_qualities, myopic_points, INTRINSIC_VALUE_MULTIPLIER, TIME_COST_MULTIPLIER)
+            myopic_quality = myopic_records[-1]['q']
+            myopic_point = myopic_records[-1]['t']
+            myopic_comprehensive_value = computation.get_time_dependent_utility(myopic_quality, myopic_point, INTRINSIC_VALUE_MULTIPLIER, TIME_COST_MULTIPLIER)
             myopic_losses.append(utils.get_percent_error(optimal_comprehensive_value, myopic_comprehensive_value))
 
             print("Nonmyopic Monitor:")
             _, nonmyopic_records = monitor.nonmyopic_monitor(tsp.k_opt_solve, quality_estimator, profile_2, profile_3, CONFIG, states, start_state, 1000)
-            nonmyopic_comprehensive_value = computation.get_time_dependent_utility(nonmyopic_records[-1]['q'], nonmyopic_records[-1]['t'], INTRINSIC_VALUE_MULTIPLIER, TIME_COST_MULTIPLIER)
+            nonmyopic_qualities = [record['q'] for record in nonmyopic_records]
+            nonmyopic_points = [record['t'] for record in nonmyopic_records]
+            nonmyopic_comprehensive_values = computation.get_time_dependent_utility(nonmyopic_qualities, nonmyopic_points, INTRINSIC_VALUE_MULTIPLIER, TIME_COST_MULTIPLIER)
+            nonmyopic_quality = nonmyopic_records[-1]['q']
+            nonmyopic_point = nonmyopic_records[-1]['t']
+            nonmyopic_comprehensive_value = computation.get_time_dependent_utility(nonmyopic_quality, nonmyopic_point, INTRINSIC_VALUE_MULTIPLIER, TIME_COST_MULTIPLIER)
             nonmyopic_losses.append(utils.get_percent_error(optimal_comprehensive_value, nonmyopic_comprehensive_value))
 
             print("Projected Monitor:")
             _, projected_records = monitor.projected_monitor(tsp.k_opt_solve, quality_estimator, CONFIG, states, start_state, 1000)
-            projected_comprehensive_value = computation.get_time_dependent_utility(projected_records[-1]['q'], projected_records[-1]['t'], INTRINSIC_VALUE_MULTIPLIER, TIME_COST_MULTIPLIER)
+            projected_qualities = [record['q'] for record in projected_records]
+            projected_points = [record['t'] for record in projected_records]
+            projected_comprehensive_values = computation.get_time_dependent_utility(projected_qualities, projected_points, INTRINSIC_VALUE_MULTIPLIER, TIME_COST_MULTIPLIER)
+            projected_quality = projected_records[-1]['q']
+            projected_point = projected_records[-1]['t'] 
+            projected_comprehensive_value = computation.get_time_dependent_utility(projected_quality, projected_point, INTRINSIC_VALUE_MULTIPLIER, TIME_COST_MULTIPLIER)
             projected_losses.append(utils.get_percent_error(optimal_comprehensive_value, projected_comprehensive_value))
+            
+            plt.figure(figsize=(16, 12), dpi=80)
+            plt.title('Performance Profile')
+            plt.xlabel('Time')
+            plt.ylabel('Value')
+
+            plt.annotate('%d-TSP' % TOUR_SIZE, xy=(0, 0), xytext=(10, 172), va='bottom', xycoords='axes fraction', textcoords='offset points')
+            plt.annotate('%d Discrete Solution Qualities' % SOLUTION_QUALITY_CLASS_COUNT, xy=(0, 0), xytext=(10, 162), va='bottom', xycoords='axes fraction', textcoords='offset points')
+            plt.annotate('$q(s) = Length_{MST} / Length(s)$', xy=(0, 0), xytext=(10, 152), va='bottom', xycoords='axes fraction', textcoords='offset points')
+            plt.annotate('$U_C(t) = -e^{%.2ft}$' % TIME_COST_MULTIPLIER, xy=(0, 0), xytext=(10, 135), va='bottom', xycoords='axes fraction', textcoords='offset points')
+            plt.annotate('$U_I(q) = %dq$' % INTRINSIC_VALUE_MULTIPLIER, xy=(0, 0), xytext=(10, 125), va='bottom', xycoords='axes fraction', textcoords='offset points')
+            plt.annotate('$U(q, t) = U_C(t) - U_I(q)$', xy=(0, 0), xytext=(10, 115), va='bottom', xycoords='axes fraction', textcoords='offset points')
+
+            plt.plot(true_steps, true_comprehensive_values, color='g', zorder=3, label='True Comprehensive Values')
+            plt.plot(fixed_points, fixed_comprehensive_values, color='g', zorder=3, label='Fixed Comprehensive Values')
+            plt.plot(myopic_points, myopic_comprehensive_values, color='g', zorder=3, label='Myopic Comprehensive Values')
+            plt.plot(nonmyopic_points, nonmyopic_comprehensive_values, color='g', zorder=3, label='Nonmyopic Comprehensive Values')
+            plt.plot(projected_points, projected_comprehensive_values, color='g', zorder=3, label='Projected Comprehensive Values')
+
+            plt.scatter([optimal_stopping_point], optimal_comprehensive_value, color='limegreen', zorder=4, label='Optimal Stopping Point')
+            plt.scatter([projected_point], projected_comprehensive_value, color='m', zorder=4, label='Projected Stopping Point')
+            plt.scatter([nonmyopic_point], nonmyopic_comprehensive_value, color='maroon', zorder=4, label='Nonmyopic Stopping Point')
+            plt.scatter([myopic_point], myopic_comprehensive_value, color='y', zorder=4, label='Myopic Stopping Point')
+            plt.scatter([fixed_point], fixed_comprehensive_value, color='c', zorder=4, label='Fixed Stopping Point')
+
+            plt.legend(bbox_to_anchor=(0.0, 1.04, 1.0, 0.102), loc=3, ncol=3, mode='expand', borderaxespad=0.0)
+
+            filename = 'plots/' + instance + '.png'
+            plt.savefig(filename)
+            plt.close()
         
 
     print("Fixed Time Allocation Average Percent Error: %f%%" % np.average(fixed_losses))
