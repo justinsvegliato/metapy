a5bd15dd2f43f5c40439d45b8231dc09e1b58cb6
Justin Svegliato <justin.svegliato1@marist.edu>
2017-08-26 13:18:56 -0400

    Implementing a new GA

--- jsp.py
+++ jsp.py
@@ -3,11 +3,44 @@ import random
 import matplotlib.pyplot as plt
 import copy
 
+GENERATIONS = 500
+POPULATION_SIZE = 500
+ELITE_PROPORTION = 0.05
+CROSSOVER_RATE = 0.9
+MUTATION_RATE = 0.1
+
 
 def get_location(assignment, job_id):
     for machine_id, job_ids in enumerate(assignment):
         if job_id in job_ids:
             return machine_id, job_ids.index(job_id)
+
+
+# def InitPopulation(ps, I):
+#     """Generate initial population from random shuffles of the tasks."""
+#     gene = [j for j in the number of jobs for t in I[j]]
+#     population = []
+#     for i in xrange(ps):
+#         shuffle(gene)
+#         population.append([j for j in gene])
+#     return population
+
+
+def load_instance(filename):
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+
+        jobs = []
+        for line in lines[3:]:
+            entries = [int(entry) for entry in line.strip().split(' ')]
+            jobs.append([{'machine_id': entries[i], 'duration': entries[i + 1]} for i in range(0, len(entries), 2)])
+
+        return {
+            'num_machines': int(lines[0]),
+            'num_jobs': int(lines[1]),
+            'job_size': int(lines[2]),
+            'jobs': jobs
+        }
 
 
 def get_random_assignment(num_machines, num_jobs):
@@ -41,6 +74,33 @@ def get_mutated_assignment(assignment, num_machines, num_jobs):
     return new_assignment
 
 
+def get_crossed_assignment(p1, p2, I):
+    def Index(p1, I):
+        ct = [0 for j in xrange(I.n)]
+        s = []
+        for i in p1:
+            s.append((i, ct[i]))
+            ct[i] = ct[i] + 1
+        return s
+
+    idx_p1 = Index(p1, I)
+    idx_p2 = Index(p2, I)
+    nt = len(idx_p1) # total number of tasks
+    i = randint(1, nt)
+    j = randint(0, nt-1)
+    k = randint(0, nt)
+    implant = idx_p1[j:min(j+i,nt)] + idx_p1[:i - min(j+i,nt) + j]
+
+    lft_child = idx_p2[:k]
+    rgt_child = idx_p2[k:]
+    for jt in implant:
+        if jt in lft_child: lft_child.remove(jt)
+        if jt in rgt_child: rgt_child.remove(jt)
+
+    child = [ job for (job, task) in lft_child + implant + rgt_child ]
+    return child
+
+
 def get_random_costs(num_machines, num_jobs):
     return np.random.rand(num_machines, num_jobs)
 
@@ -69,44 +129,44 @@ def get_elite_population(population, costs, size):
 
 
 def main():
-    num_machines = 5
-    num_jobs = 50
+    # num_machines = 5
+    # num_jobs = 50
 
-    costs = get_random_costs(num_machines, num_jobs)
-    # costs = np.matrix([[0.97188071, 0.05795095, 0.19207029, 0.3883632, 0.27602015, 0.4979248, 0.24620109, 0.73204223, 0.24851216, 0.66713638], 
-    #                 [0.99284099, 0.54011251, 0.86960698, 0.40496969, 0.16749654, 0.58906524, 0.01747369, 0.89810728, 0.37468181, 0.86894554], 
-    #                 [0.3420342, 0.34764302, 0.0380342, 0.64255261, 0.85022931, 0.08053618, 0.44643143, 0.94173863, 0.64872432, 0.05583479], 
-    #                 [0.81696921, 0.82957557, 0.74362754, 0.0137083, 0.13724315, 0.49883214, 0.97145286, 0.33193399, 0.17220811, 0.48126264], 
-    #                 [0.78329609, 0.43502877, 0.07277983, 0.55379149, 0.21066182, 0.63106304, 0.61506724, 0.29250189, 0.24160404, 0.67692696]])
+    # costs = get_random_costs(num_machines, num_jobs)
 
-    generations = 1000
-    population_size = 1000
-    elite_population_size = int(0.9 * population_size)
+    # generations = 400
+    # population_size = 1000
+    # elite_population_size = int(0.9 * population_size)
 
-    qualities = []
+    # qualities = []
 
-    population = [get_random_assignment(num_machines, num_jobs) for i in range(population_size)]
+    # population = [get_random_assignment(num_machines, num_jobs) for i in range(population_size)]
 
-    for generation in range(generations):
-        new_population = get_elite_population(population, costs, elite_population_size)
-        new_population = [get_mutated_assignment(assignment, num_machines, num_jobs) for assignment in new_population]
+    # for generation in range(generations):
+    #     new_population = get_elite_population(population, costs, elite_population_size)
+    #     new_population = [get_mutated_assignment(assignment, num_machines, num_jobs) for assignment in new_population]
 
-        sorted_population = get_sorted_population(population, costs)
-        sorted_population[-elite_population_size:] = new_population
+    #     sorted_population = get_sorted_population(population, costs)
+    #     sorted_population[-elite_population_size:] = new_population
 
-        population = sorted_population
+    #     population = sorted_population
 
-        quality = 1.5 / get_total_cost(population[0], costs)
-        qualities.append(quality)
+    #     quality = 1.5 / get_total_cost(population[0], costs)
+    #     qualities.append(quality)
 
-        print(generation, quality)
+    #     print(generation, quality)
 
-    plt.figure()
-    plt.title('Performance Profile')
-    plt.xlabel('Time')
-    plt.ylabel('Costs')
-    plt.scatter(range(len(qualities)), qualities)
-    plt.show()
+    # with open('q.txt') as f:
+    #     qualities = [5000 / float(quality) for quality in f.readlines()]
+
+    # plt.figure()
+    # plt.title('Performance Profile')
+    # plt.xlabel('Time')
+    # plt.ylabel('Costs')
+    # plt.scatter(range(len(qualities)), qualities)
+    # plt.show()
+
+    print(load_instance('q.txt'))
 
 
 if __name__ == '__main__':
